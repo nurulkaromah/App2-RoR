32789af31a611ccd6d588d740186b162ff52fa51
Salman <ahmad.farisi42@gmail.com>
2020-09-15 10:22:29 +0700

    Menambah comment di routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  #percobaan untuk membuat route di ror 
+  #coba untuk push di git
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
